7988b1003b51a39dd0537bac61e5f4be605c1ac8
Shivam Tiwari <shivamtiwari6223@gmail.com>
2024-08-18 22:22:45 +0530

    fixed the logo bug

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,12 @@
 import "../Styles/Header.css"
 import {Link} from 'react-router-dom'
+ 
+import logo from "../assets/kmclu-logo.png"
+
 export default function Header() {
   return (
     <div id="header">
-        <img src=".\src\assets\kmclu-logo.png" alt="KMCLU image" />
+        <img src= {logo} alt="KMCLU image" />
         <span><Link  to={'/'} >Home</Link></span>
         <span><Link  to={'/Upload'} >Upload</Link></span>
         <span><Link  to={'/Discussion'} >Discussion</Link></span>
