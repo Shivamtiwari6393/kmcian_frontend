0348cd00a912c9cd46d215c66e429e11dd3ee14c
Shivam Tiwari <shivamtiwari6223@gmail.com>
2024-08-22 22:46:55 +0530

    changed url

--- src/Components/Update.jsx
+++ src/Components/Update.jsx
@@ -40,8 +40,8 @@ export default function Upload() {
 
 
 
-    const url = "http://127.0.0.1:8000";
-    // const url = "https://kmcianbackend.vercel.app";
+    // const url = "http://127.0.0.1:8000";
+    const url = "https://kmcianbackend.vercel.app";
 
 
 
