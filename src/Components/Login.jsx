795e43369b10ab42bf12ac594a70d6d5d76967ab
Shivam Tiwari <shivamtiwari6223@gmail.com>
2024-09-01 17:08:51 +0530

    added loading in login.jsx

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "../Styles/Login.css";
 import userIcon from "../assets/user.png";
 import passwordIcon from "../assets/password.png";
+import Loading from "./Loading";
 
 function Login() {
   const [credentials, setCredentials] = useState({
@@ -11,6 +12,8 @@ function Login() {
 
   const [error, setError] = useState("");
 
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleInputChange = (e) => {
     setCredentials((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
@@ -18,7 +21,7 @@ function Login() {
   const handleButtonClick = () => {
     // const url = "http://127.0.0.1:8000/api/login";
     const url = "https://kmcianbackend.vercel.app/api/login";
-
+    setIsLoading(true);
     fetch(url, {
       method: "POST",
       body: JSON.stringify(credentials),
@@ -32,57 +35,62 @@ function Login() {
         return res.json();
       })
       .then((data) => {
-        // if login successfull save the token 
+        // if login successfull save the token
         localStorage.setItem("kmciantoken", data.token);
         setError("Login Succesfull");
+        setIsLoading(false);
       })
       .catch((e) => {
+        setIsLoading(false);
         setError(e.message);
       });
   };
 
   return (
-    <div className="login-container">
-      {error && (
-        <div className="error-container">
-          <p>{error}</p>
+    <>
+      {isLoading && <Loading></Loading>}
+      <div className="login-container">
+        {error && (
+          <div className="error-container">
+            <p>{error}</p>
+          </div>
+        )}
+        <div className="login-header">
+          <h4>Login</h4>
         </div>
-      )}
-      <div className="login-header">
-        <h4>Login</h4>
-      </div>
-      <div className="login-fields">
-        <div className="username-container">
-          <label htmlFor="username">
-            <img src={userIcon} alt="user" />
-          </label>
-          <input
-            type="text"
-            placeholder="Username/Email"
-            name="email"
-            value={credentials.email}
-            onChange={handleInputChange}
-          />
-        </div>
+        <div className="login-fields">
+          <div className="username-container">
+            <label htmlFor="username">
+              <img src={userIcon} alt="user" />
+            </label>
+            <input
+              type="text"
+              placeholder="Username/Email"
+              name="email"
+              value={credentials.email}
+              onChange={handleInputChange}
+            />
+          </div>
 
-        <div className="password-container">
-          <label htmlFor="password">
-            <img src={passwordIcon} alt="password" />
-          </label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={credentials.password}
-            onChange={handleInputChange}
-          />
-        </div>
+          <div className="password-container">
+            <label htmlFor="password">
+              <img src={passwordIcon} alt="password" />
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={credentials.password}
+              onChange={handleInputChange}
+            />
+          </div>
 
-        <div className="login-button-container">
-          <button onClick={handleButtonClick}>Login</button>
+          <div className="login-button-container">
+            <button onClick={handleButtonClick}>Login</button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
