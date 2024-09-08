4971ca71bf67f1525ba93325132989bbf2ac7337
Shivam Tiwari <shivamtiwari6223@gmail.com>
2024-09-08 14:15:02 +0530

    removed 2019 2020 in card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -192,8 +192,8 @@ export default function Card() {
 
   const yearOptions = [
     { value: "All", label: "All" },
-    { value: "2019", label: "2019" },
-    { value: "2020", label: "2020" },
+    // { value: "2019", label: "2019" },
+    // { value: "2020", label: "2020" },
     { value: "2021", label: "2021" },
     { value: "2022", label: "2022" },
     { value: "2023", label: "2023" },
