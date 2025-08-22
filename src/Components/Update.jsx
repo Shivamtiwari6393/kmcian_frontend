87cfd5b0954f2d60e9d8c3b72b62fccc80052f65
Shivam Tiwari <shivamtiwari6223@gmail.com>
2025-08-22 18:32:50 +0530

    fixed update paper ui

--- src/Components/Update.jsx
+++ src/Components/Update.jsx
@@ -8,7 +8,11 @@ import CustomSelect from "./CustomSelect";
 import toast from "react-hot-toast";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import data from "./data.js"
+
 export default function Upload() {
+  const  [yearOptions,semesterOptions,socialSciencesOptins, artHumnanitiesOptions, pharmacyBranchOptions, scienceBranchOptions, legalStudiesBranchOptions,commerceBranchOptions, engineeringBranchOptions, courseOptions ] = data
+
   // const url = "http://127.0.0.1:8000/api/paper";
   const url = "https://kmcianbackend.vercel.app/api/paper";
 
@@ -222,138 +226,6 @@ export default function Upload() {
       });
   };
 
-  // Options for select inputs
-  const courseOptions = [
-    { value: "Engineering", label: "Engineering" },
-    { value: "Commerce", label: "Commerce" },
-    { value: "Legal Studies", label: "Legal Studies" },
-    { value: "Science", label: "Science" },
-    { value: "Social Science", label: "Social Science" },
-    { value: "Art and Humanities", label: "Arts & Humanities" },
-    { value: "Pharmacy", label: "Pharmacy" },
-  ];
-
-  // branch options
-
-  const engineeringBranchOptions = [
-    { value: "All", label: "All" },
-    { value: "CSE(AI&ML)", label: "CSE(AI&ML)" },
-    { value: "CSE(AI&DS)", label: "CSE(AI&DS)" },
-    { value: "CSE", label: "CSE" },
-    { value: "BIO TECHNOLOGY", label: "Bio Technology" },
-    { value: "CIVIL", label: "Civil" },
-    { value: "MACHANICAL", label: "Machanical" },
-    { value: "M.Tech - CSE(AI&ML)", label: "M.Tech - CSE(AI&ML)" },
-  ];
-
-  const commerceBranchOptions = [
-    { value: "All", label: "All" },
-    { value: "BBA", label: "BBA" },
-    { value: "MBA", label: "MBA" },
-    { value: "MBA FA", label: "MBA(FA)" },
-    { value: "B.COM", label: "B.COM" },
-    { value: "B.COM(TT)", label: "B.COM(TT)" },
-    { value: "M.COM", label: "M.COM" },
-  ];
-
-  const legalStudiesBranchOptions = [
-    { value: "All", label: "All" },
-    { value: "LLM", label: "LLM" },
-    { value: "BA LLB", label: "BA LLB" },
-    { value: "LLB", label: "LLB" },
-  ];
-
-  // science options
-
-  const scienceBranchOptions = [
-    { value: "All", label: "All" },
-    { value: "MCA", label: "MCA" },
-    { value: "BCA", label: "BCA" },
-    { value: "BSc PHYSICS", label: "B.Sc Physics" },
-    { value: "BSc CHEMISTRY", label: "B.Sc Chemistry" },
-    { value: "BSc MATHEMATICS", label: "B.Sc Mathematics" },
-    { value: "BSc CS", label: "B.Sc Computer Science" },
-    { value: "BSc BIOTECHNOLOGY", label: "B.Sc Biotechnology" },
-    { value: "BSc ZOOLOGY", label: "B.Sc Zoology" },
-    { value: "BSc BOTANY", label: "B.Sc Botany" },
-    { value: "BSc MICROBIOLOGY", label: "B.Sc Microbiology" },
-    { value: "BSc STATISTICS", label: "B.Sc Statistics" },
-    { value: "BA HM", label: "BA Home Science" },
-    { value: "MA HM", label: "MA Home Science" },
-    { value: "B LIB", label: "B.lib." },
-  ];
-
-  // pharmacy options
-
-  const pharmacyBranchOptions = [
-    { value: "All", label: "All" },
-    { value: "B PHARM", label: "B.Pharm" },
-    { value: "D PHARM", label: "D.Pharm" },
-  ];
-
-  // arts and humanities options
-
-  const artHumnanitiesOptions = [
-    { value: "All", label: "All" },
-    { value: "MA ARABIC", label: "MA ARABIC" },
-    { value: "MA ENGLISH", label: "MA ENGLISH" },
-    { value: "MA HINDI", label: "MA HINDI" },
-    { value: "MA PERSIAN", label: "MA PERSIAN" },
-    { value: "MA URDU", label: "MA URDU" },
-    { value: "BA ARABIC", label: "BA ARABIC" },
-    { value: "BA ENGLISH", label: "BA ENGLISH" },
-    { value: "BA HINDI", label: "BA HINDI" },
-    { value: "BA PERSIAN", label: "BA PERSIAN" },
-    { value: "BA URDU", label: "BA URDU" },
-    { value: "BA FRENCH", label: "BA FRENCH" },
-    { value: "BA CHINESE", label: "BA CHINESE" },
-    { value: "BA GERMAN", label: "BA GERMAN" },
-    { value: "BA JAPANESE", label: "BA JAPANESE" },
-    { value: "BA SANSKRIT", label: "BA SANSKRIT" },
-    { value: "BA PALI", label: "BA PALI" },
-  ];
-
-  const socialSciencesOptins = [
-    { value: "All", label: "All" },
-    { value: "B ED", label: "B.ED" },
-    { value: "MA EDUCATION", label: "MA EDUCATION" },
-    { value: "MA JOURN_MASS_COMM", label: "MA JOURN MASS COMM" },
-    { value: "MA HISTORY", label: "MA HISTORY" },
-    { value: "MA GEOGRAPHY", label: "MA GEOGRAPHY" },
-    { value: "MA ECONOMICS", label: "MA ECONOMICS" },
-    { value: "MA FINE ARTS", label: "MA FINE ARTS" },
-    { value: "BA EDUCATION", label: "BA EDUCATION" },
-    { value: "BA HISTORY", label: "BA HISTORY" },
-    { value: "BA GEOGRAPHY", label: "BA GEOGRAPHY" },
-    { value: "BA ECONOMICS", label: "BA ECONOMICS" },
-    { value: "BA FINE ARTS", label: "BA FINE ARTS" },
-    { value: "BA POL SCIENCE", label: "BA POL SCIENCE" },
-    { value: "BA PHYSICAL EDU", label: "BA PHYSICAL EDU" },
-    { value: "BA JOURN_MASS_COMM", label: "BA JOURN MASS COMM" },
-    { value: "BA SOCIOLOGY", label: "BA SOCIOLOGY" },
-  ];
-
-  const semesterOptions = [
-    { value: "1", label: "1" },
-    { value: "2", label: "2" },
-    { value: "3", label: "3" },
-    { value: "4", label: "4" },
-    { value: "5", label: "5" },
-    { value: "6", label: "6" },
-    { value: "7", label: "7" },
-    { value: "8", label: "8" },
-  ];
-
-  const yearOptions = [
-    { value: "2019", label: "2019" },
-    { value: "2020", label: "2020" },
-    { value: "2021", label: "2021" },
-    { value: "2022", label: "2022" },
-    { value: "2023", label: "2023" },
-    { value: "2024", label: "2024" },
-    { value: "2025", label: "2025" },
-  ];
-
   let branchOptions = [];
 
   updateData.course === "Engineering"
@@ -425,6 +297,8 @@ export default function Upload() {
           onClick={() => handleSelectClick("branch")}
           onChange={(value) => handleDataChange("branch", value)}
           placeholder={updateData.branch}
+          inculudeAll = {true}
+          
         />
         <CustomSelect
           options={semesterOptions}
@@ -432,6 +306,8 @@ export default function Upload() {
           onClick={() => handleSelectClick("semester")}
           onChange={(value) => handleDataChange("semester", value)}
           placeholder={updateData.semester}
+          inculudeAll = {true}
+
         />
         <CustomSelect
           options={yearOptions}
@@ -439,6 +315,8 @@ export default function Upload() {
           onClick={() => handleSelectClick("year")}
           onChange={(value) => handleDataChange("year", value)}
           placeholder={updateData.year}
+          inculudeAll = {true}
+
         />
 
         <div className={uploadcss["name"]}>
@@ -478,10 +356,9 @@ export default function Upload() {
           <p>U: {new Date(updateData.updatedAt).toLocaleString()}</p>
         </div>
 
-        <button onClick={update}>Update</button>
-        {/* <button id={uploadcss["delete-button"]} onClick={handleDelete}>
-          Delete
-        </button> */}
+        <button id={uploadcss["update-button"]} onClick={update}>
+          Update
+        </button>
 
         <div className={uploadcss["delete-button-container"]}>
           <FontAwesomeIcon
